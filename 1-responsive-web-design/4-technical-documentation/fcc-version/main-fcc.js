6d264a2e1c483dd00e7dbc5e798953a19298921f
Ahmed A Gadir <ahmed.gadir@gmail.com>
2018-07-31 01:24:38 +0100

    update

---
+++ 1-responsive-web-design/4-technical-documentation/fcc-version/main-fcc.js
@@ -0,0 +1,80 @@
+/* ==== bootstrap 4 ad ====== */
+document.querySelector('.b4-ad').addEventListener('click', e => {
+  e.target.style.display = 'none';
+})
+
+/* ===== sandwhich button functionality ====== */
+
+// document.querySelector('nav button.sandwhich').addEventListener('click', () => {
+//   let classList = document.querySelector('nav .nav-links ul').classList;
+
+//   Array.from(classList).includes('visible') ? classList.remove('visible') : classList.add('visible');
+// })
+
+
+/* ========== SIDEBAR ======== */
+
+let sections = Array.from(document.querySelector('.documentation').children)
+
+window.addEventListener('scroll', () => {
+
+	sections.forEach(section => {
+    let sectionID = section.getAttribute('id');
+  
+  	if (section.getBoundingClientRect().bottom >= 0 && section.getBoundingClientRect().bottom <= section.offsetHeight) {
+    
+      document.querySelector(`[href="#${sectionID}"]`).parentElement.classList.add('active');
+      
+      Array.from(document.querySelector(`[href="#${sectionID}"]`).parentElement.parentElement.children).forEach(child => child.style.display = 'block')
+      
+      let subsections = Array.from(document.querySelectorAll(`#${sectionID} > div`));
+      
+      subsections.forEach(subsection => {
+        if (subsection.getBoundingClientRect().bottom >= 0 && subsection.getBoundingClientRect().bottom <= subsection.offsetHeight) {
+          let subsectionID = subsection.getAttribute('id');
+  				
+          document.querySelector(`[href="#${subsectionID}"]`).parentElement.classList.add('active');
+          
+        } else {
+        	document.querySelector(`[href="#${subsection.getAttribute('id')}"]`).parentElement.classList.remove('active');
+        }
+           
+      })
+          
+    } else {
+    	document.querySelector(`[href="#${section.getAttribute('id')}"]`).parentElement.classList.remove('active');
+      Array.from(document.querySelector(`[href="#${sectionID}"]`).parentElement.parentElement.children).forEach((child, index) => {
+      if (index != 0) 
+      	child.style.display = 'none';
+      });
+    }
+  })
+})
+
+
+
+//  =========== BACK TO TOP -==============
+
+document.getElementById('back_to_top').addEventListener('click', () => {    
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+})
+
+//  =========== links -==============
+
+let allHeadings = [...document.querySelectorAll('.documentation > section > header'),...document.querySelectorAll('.documentation > section > div > h3')];
+
+allHeadings.forEach(heading => {
+  heading.addEventListener('mouseover', () => heading.firstElementChild.style.opacity = '1');
+  heading.addEventListener('mouseout', () => heading.firstElementChild.style.opacity = '0');
+  heading.firstElementChild.addEventListener('click', () => window.location = `index.html#${heading.textContent.toLowerCase().replace(/\s/g, '_')}`)
+});
+
+// ========== writing codeblocks =======
+
+function escapeHTML (str) {
+  str = str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,'&#39;');
+  return str
+}
+
+console.log(escapeHTML('<h1>h1. Bootstrap heading <small>Secondary text</small></h1>'))
